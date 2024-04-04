6d8ca9ff168410958086ccd894a1ff08e96d35d7
emircsanli <emircsanli@gmail.com>
2024-04-04 04:01:56 +0300

    Lab6 last commit; Room Data, Reservation, Reservation Handler classes were added. Add, delete and display functions were written and edited.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ public class RoomData
     [JsonPropertyName("Room")]
     public Room[] Rooms { get; set; }
 }
-
 public class Room
 {
     [JsonPropertyName("roomId")]
@@ -20,67 +19,178 @@ public class Room
     public int capacity { get; set; }
 }
 
-public class Reservation{
-    
-    public DateTime time;
-    public DateTime date;
-    public string reserverName;
-    public Room room;
+public class Reservation
+{
+    public DateTime time { get; set; }
+    public DateTime date { get; set; }
+    public string reserverName { get; set; }
+    public Room room { get; set; }
 }
 
-public class ReservationHandler{
+public class ReservationHandler
+{
+    public Reservation[,] reservations { get; set; } = new Reservation[10, 7];
 
 
+    public void addReservation(Reservation reservation)
+    {
+
+        bool reservationAdded = false;
+
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (reservations[j, i] == null)
+                {
+                    reservations[j, i] = reservation;
+                    reservationAdded = true;
+                    break;
+                }
+            }
+            if (reservationAdded)
+                break;
+        }
+
+        if (reservationAdded)
+        {
+            Console.WriteLine("Your reservation successfully added: " + reservation.reserverName + " to " + reservation.room.roomId);
+        }
+        else
+        {
+            Console.WriteLine("Weekly schedule is full.");
+        }
+    }
+
+    public void deleteReservation(Reservation reservation)
+    {
+        bool reservationDeleted = false;
+
+        for (int row = 0; row < 10; row++)
+        {
+            for (int column = 0; column < 7; column++)
+            {
+                if (reservations[row, column] != null && reservation == reservations[row, column])
+                {
+                    reservations[row, column] = null;
+                    reservationDeleted = true;
+                }
+            }
+        }
+
+        if (reservationDeleted)
+        {
+            Console.WriteLine(reservation.room.roomId + "-" +reservation.reserverName + "'s reservation deleted.");
+        }
+        else
+        {
+            Console.WriteLine("Reservation not found.");
+        }
+    }
+
+    public void displayWeeklySchedule()
+    {
+        Console.WriteLine("Weekly Reservation Schedule:");
+        Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------------");
+        Console.Write("Time\t");
+
+        string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        foreach (var day in daysOfWeek)
+        {
+            Console.Write(day.PadRight(20));
+        }
+        Console.WriteLine();
+        Console.Write("----------------------------------------------------------------------------------------------------------------------------------------");
+        Console.WriteLine();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"{(i + 9).ToString("00")}:00|\t");
+            for (int j = 0; j < 7; j++)
+            {
+                string savedReservation = reservations[i, j]?.reserverName ?? " ";
+                Console.Write(savedReservation.PadRight(20));
+            }
+            Console.WriteLine();
+        }
+        Console.Write("----------------------------------------------------------------------------------------------------------------------------------------");
+        Console.WriteLine();
+    }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(String[] args)
     {
-        int[] a=new int [2];
-        a=[1,2];
 
-for(int i=0;i<2;i++){
-    Console.Write(a[i]);
-}
-        int[,]array = new int[2,2]{ { 1, 2 }, { 2, 3 } };
+        //define file path
+        string filePath = "Data.json";
 
-        //path to json into string
-        //to do inside try catch
-        string jsonFilePath = "Data.json";
-
-        //options to read
-        string jsonString = File.ReadAllText(jsonFilePath);
-
-        var options = new JsonSerializerOptions()
-        {
-            NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.WriteAsString
-        };
-
-        //read try catch
-        var roomData = JsonSerializer.Deserialize<RoomData>(jsonString, options); //full string aldığımız datayı anlamlandıracak fonksiyon
+        //Read from json
+        // 1 -> json to text
+        string jsonString = File.ReadAllText(filePath);
+        // 2 -> decode text into meaningful classes
+        var roomData = JsonSerializer.Deserialize<RoomData>(
+                                jsonString,
+                                new JsonSerializerOptions()
+                                {
+                                    NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.WriteAsString
+                                });
 
 
+        //print
         if (roomData?.Rooms != null)
         {
-            //print
             foreach (var room in roomData.Rooms)
             {
-
-                Console.WriteLine($"Room ID: {room.roomId}, Name: {room.roomName}, Capacity {room.capacity}");
+                Console.WriteLine($"Room ID : {room.roomId} RoomName : {room.roomName} Capacity : {room.capacity}");
             }
-
         }
+        ReservationHandler reservationHandler = new ReservationHandler();
+
+        Reservation[] reservationsArray = new Reservation[70];
+        Random random = new Random();
+
+        for (int i = 0; i < reservationsArray.Length; i++)
+        {
+            Reservation newReservation = new Reservation();
+            newReservation.time = DateTime.Now;
+            newReservation.date = DateTime.Today;
+            newReservation.reserverName = $"Customer-{i + 1}";
+            newReservation.room = roomData.Rooms[random.Next(roomData.Rooms.Length)];
+
+            reservationsArray[i] = newReservation;
+            reservationHandler.addReservation(newReservation);
+        }
+        Console.WriteLine("Rezervations added randomly rooms)");
+        Console.WriteLine();
+
+        reservationHandler.displayWeeklySchedule();
+
+       for (int i = 0; i < 5; i++)
+{
+    int randomIndex = random.Next(reservationsArray.Length);
+    Reservation reservationToDelete = reservationsArray[randomIndex];
+
+    if ( reservationsArray[randomIndex]!= null)
+    {
+        reservationHandler.deleteReservation(reservationToDelete);
+        reservationsArray[randomIndex] = null; 
+    }
+    else
+    {
+        Console.WriteLine("Previously deleted.");
         
-        for(int i = 0 ; i<2 ; i++){
+    }
+}
 
-            for(int j = 0 ; j<2 ; j++){
+Console.WriteLine("5 random reservations deleted");
+Console.WriteLine();
 
-                Console.Write(array[i,j]);
-            }
-            
-            Console.WriteLine(" ");
-        }
+reservationHandler.displayWeeklySchedule();
+
+     
 
     }
 }
